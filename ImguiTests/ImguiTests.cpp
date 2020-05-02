32f6be7d51d59c3f02b29f4c67c7e7a75fa6e3f3
Pawel Drzycimski <pdrzycimski@gmail.com>
2020-05-02 21:21:18 +0200

    Clear frame before starting drawing new one.

--- ImguiTests/ImguiTests.cpp
+++ ImguiTests/ImguiTests.cpp
@@ -176,23 +176,24 @@ int main(int argc, char *argv[])
     glfwPollEvents();
 //  processInput(window);
 
+    //glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
     // feed inputs to dear imgui, start new frame
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
-
-    ImGui::ShowDemoWindow();
+//    ImGui::ShowDemoWindow();
+    ImGui::ShowMetricsWindow();
    
-
     // Render dear imgui into screen
     ImGui::Render();
 
     int display_w, display_h;
     glfwGetFramebufferSize(window, &display_w, &display_h);
     glViewport(0, 0, display_w, display_h);
-    //glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
+ 
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     glfwSwapBuffers(window);
