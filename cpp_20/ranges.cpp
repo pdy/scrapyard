8ce41c698b94b8b95156b576f5bb78b667373846
pdy <pdrzycimski@gmail.com>
2022-01-14 22:49:33 +0100

    sort

--- cpp_20/ranges.cpp
+++ cpp_20/ranges.cpp
@@ -1,78 +1,19 @@
-/*
-*  MIT License
-*  
-*  Copyright (c) 2020 Pawel Drzycimski
-*  
-*  Permission is hereby granted, free of charge, to any person obtaining a copy
-*  of this software and associated documentation files (the "Software"), to deal
-*  in the Software without restriction, including without limitation the rights
-*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*  copies of the Software, and to permit persons to whom the Software is
-*  furnished to do so, subject to the following conditions:
-*  
-*  The above copyright notice and this permission notice shall be included in all
-*  copies or substantial portions of the Software.
-*  
-*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-*  SOFTWARE.
-*
-*/
-
-#include <cmdline.h> 
-#include "simplelog/simplelog.h"
-
-#include <iterator>
 #include <ranges>
-#include <vector>
+#include <algorithm>
+#include <concepts>
 
-int main(int argc, char *argv[])
+#include "simplelog/simplelog.h"
+#include "cmdline.h"
+
+int main()
 {
-  cmdline::parser arg;
-  arg.add("help", 'h', "Print help.");
-//  arg.add<std::string>("file", 'f', "Example file argument.", true);
-    
-  if(!arg.parse(argc, const_cast<const char* const*>(argv)))
-  {
-    const auto fullErr = arg.error_full();
-    if(!fullErr.empty())
-      log << fullErr;
-     
-    log << arg.usage();
-    return 0;
-  }
   
-  if(arg.exist("help"))
-  {
-    log << arg.usage();
-    return 0;
-  } 
+  std::vector nums {1, 5, 9, 3, 1, 50, 49};
+  log << "nums " << nums;
 
-  /* Example file arg check and get
-  if(!arg.exist("file"))
-  {
-    log << "--file or -f argument is mandatory!";
-    log << arg.usage();
-    return 0;
-  }
+  std::ranges::sort(nums);
+
+  log << "nums sorted " << nums; 
   
-  const std::string file = arg.get<std::string>("file");
-  */
-
-
-
-  std::vector<int> integers { 11, 2, 5, 4};
-  log << "Not sorted: " << integers; 
-  std::ranges::sort(integers);
-  log << "Sorted: " << integers; 
-
-  std::ranges::reverse(integers);
-  log << "Reversed: " << integers;
-
- 
   return 0;
 }
