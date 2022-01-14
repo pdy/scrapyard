8b84cac60ac6a0d984d16fd959a640e217f592ae
pdy <pdrzycimski@gmail.com>
2022-01-14 23:02:51 +0100

    bs

--- cpp_20/ranges.cpp
+++ cpp_20/ranges.cpp
@@ -8,12 +8,16 @@
 int main()
 {
   
-  std::vector nums {1, 5, 9, 3, 1, 50, 49};
+  std::vector nums = {1, 5, 9, 3, 1, 50, 49};
   log << "nums " << nums;
 
   std::ranges::sort(nums);
 
   log << "nums sorted " << nums; 
-  
+
+  // this stuff does not work in clang 12
+//  auto reversed = nums | std::views::reverse;
+//  auto reversed_2 = std::ranges::reverse_view(nums);
+
   return 0;
 }
