390bf7facec4a58328d779238a387ba64b87d523
pdy <pdrzycimski@gmail.com>
2022-04-10 22:20:57 +0200

    Try JSDoc

--- node_addon_tests/lib/bindings.js
+++ node_addon_tests/lib/bindings.js
@@ -1,14 +1,33 @@
+'use strict';
+
 const addon = require('../build/Release/hello_addon.node')
 
+/**
+ * @returns {string}
+ */
 function sayHi(){
   return addon.sayHi();
 }
 
+/**
+ * @param {string} str
+ * @param {number} posNum
+ * @param {function} callback
+ * @returns {string}
+ */
 function helloSubstr(str, posNum, callback) {
   return addon.helloSubstr(str, posNum, callback);
 }
 
+/**
+ * @param {string} str
+ */
 function NativeClassStrDemoWrap(str){
+  
+  /**
+   * @param {number} num
+   * @returns {string}
+  */
   this.substr = function(num){
     return _instance.substr(num);
   }
