27e9464194fc3b6b77fd31dfe98795c596442fb5
pdy <pdrzycimski@gmail.com>
2025-01-04 20:03:15 +0100

    Hello

---
+++ RitsOrderCache/OrderCacheImpl.cpp
@@ -0,0 +1,4 @@
+#include "OrderCacheImpl.h"
+
+
+
---
+++ RitsOrderCache/OrderCacheImpl.h
@@ -0,0 +1,27 @@
+#include "OrderCache.h"
+
+class OrderCacheImpl : public OrderCacheInterface
+{
+public:
+   ~OrderCacheImpl() = default;
+
+  // add order to the cache
+   void addOrder(Order order) override;
+
+  // remove order with this unique order id from the cache
+   void cancelOrder(const std::string& orderId) override; 
+
+  // remove all orders in the cache for this user
+   void cancelOrdersForUser(const std::string& user) override; 
+
+  // remove all orders in the cache for this security with qty >= minQty
+   void cancelOrdersForSecIdWithMinimumQty(const std::string& securityId, unsigned int minQty) override; 
+
+  // return the total qty that can match for the security id
+   unsigned int getMatchingSizeForSecurity(const std::string& securityId) override; 
+
+  // return all orders in cache in a vector
+   std::vector<Order> getAllOrders() const override;  
+
+
+};
