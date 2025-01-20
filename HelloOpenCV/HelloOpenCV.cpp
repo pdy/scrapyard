395f3c0c8ef4a8cd6d756fa1db129b1e0ac40737
pdy <pdrzycimski@gmail.com>
2025-01-20 01:53:41 +0100

    Hello opencv

---
+++ HelloOpenCV/HelloOpenCV.cpp
@@ -0,0 +1,77 @@
+/*
+*  MIT License
+*  
+*  Copyright (c) 2020 Pawel Drzycimski
+*  
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*  
+*  The above copyright notice and this permission notice shall be included in all
+*  copies or substantial portions of the Software.
+*  
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*  SOFTWARE.
+*
+*/
+
+#include <cmdline.h> 
+#include "simplelog/simplelog.h"
+
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char *argv[])
+{
+  
+  cmdline::parser arg;
+  arg.add("help", 'h', "Print help.");
+  arg.add<std::string>("file", 'f', "File", true);
+    
+  if(!arg.parse(argc, const_cast<const char* const*>(argv)))
+  {
+    const auto fullErr = arg.error_full();
+    if(!fullErr.empty())
+      LOG << fullErr;
+     
+    LOG << arg.usage();
+    return 0;
+  }
+  
+  if(arg.exist("help"))
+  {
+    LOG << arg.usage();
+    return 0;
+  } 
+
+  if(!arg.exist("file"))
+  {
+    LOG << "--file or -f argument is mandatory!\n";
+    LOG << arg.usage();
+    return 0;
+  }
+  
+  const std::string file = arg.get<std::string>("file");
+  
+  const auto imageMat = cv::imread(file);
+
+  if(imageMat.empty())
+  {
+    LOG << "Failed to read ";
+  }
+  else
+  {
+    LOG << "Image height [" << imageMat.size().height << "] width [" << imageMat.size().width << "]";
+    cv::imshow("HelloOpenCV", imageMat);
+    cv::waitKey();
+  }
+
+  return 0;
+}
