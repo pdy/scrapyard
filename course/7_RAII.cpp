c75a882b717bd555d5914ca8ff54a591938e2a9b
pawel.drzycimski <pawel.drzycimski@intel.com>
2025-08-26 12:28:52 +0200

    Push code written live

--- course/7_RAII.cpp
+++ course/7_RAII.cpp
@@ -1,19 +1,22 @@
 #include "simplelog/simplelog.hpp"
-//#include <memory>
+#include <mutex>
+#include <type_traits>
 
-#if 0
 template<typename T, auto Deleter>
 struct GenericDeleter
 {
   void operator()(T *rc) const
   {
+    LOG << "generic deleter";
     Deleter(rc);
   }
 };
 
 template<typename T, auto Func>
 using legacy_ptr = std::unique_ptr<T, GenericDeleter<T, Func>>;
-#endif
+
+
+// Resource Acquisition Is Initialization
 
 struct StackUnwind
 {
@@ -35,27 +38,83 @@ static void throw_exception()
   StackUnwind s4 { 8 };
 }
 
+template<typename T>
+class ScopeGuard
+{
+  T func_;
+public:
+  ScopeGuard(T func)
+    : func_(std::move(func))
+  {
+    static_assert(std::is_invocable<T>::value);
+  }
+
+  ~ScopeGuard()
+  {
+    func_();
+  }
+};
+
+template<typename T>
+[[nodiscard]] ScopeGuard<T> makeScopeGuard(T callable)
+{
+  return ScopeGuard<T>(std::move(callable));
+}
+
+
+using File_uptr = std::unique_ptr<std::FILE, decltype(&std::fclose)>;
+
+using File_uptr_2 = legacy_ptr<std::FILE, std::fclose>;
+
+struct FileStream
+{
+  File_uptr_2 file_;
+
+  
+  void setFIlename();
+  bool streamTo(std::ostream &ss);
+};
+
 int main()
 {
+  size_t counter = 5;
+  LOG << "counter " << counter;
+  {
+    const auto decreaseCounter = makeScopeGuard([&counter]
+    {
+      --counter;
+    });
+  }
+  LOG << "counter " << counter;
 
-  StackUnwind s0 { 0 };
+
+  std::FILE *file {nullptr};
+  const auto fileGuard = makeScopeGuard([&] { if(file) std::fclose(file);});
+
+  if(file)
+    std::fclose(file);
+
+
+  std::mutex mtx;
   {
-    StackUnwind s1 { 1 };
-    StackUnwind s2 { 2 };
-    StackUnwind s3 { 3 };
-    StackUnwind s4 { 4 };
+    std::lock_guard<std::mutex> lock (mtx);
   }
-  StackUnwind s7 { 7 };
-  /*
-  try{
-    throw_exception();
-    StackUnwind s3 { 3 };
-    StackUnwind s4 { 4 };
-  }
-  catch(const char *str)
-  {
-    LOG << "Catched exception " << str;
-  }
-*/
+
+  
+//  StackUnwind s1 { 1 };
+  /*StackUnwind s2 { 2 };
+  StackUnwind s3 { 3 };
+  StackUnwind s4 { 4 };
+  */
+ // try{
+//    throw_exception();
+  //  StackUnwind s3 { 3 };
+  //  StackUnwind s4 { 4 };
+ // }
+//  catch(const char *str)
+//  {
+//    LOG << "Catched exception " << str;
+//  }
+
   return 0;
 }
--- course/8_uptr.cpp
+++ course/8_uptr.cpp
@@ -1,34 +1,79 @@
 #include "simplelog/simplelog.hpp"
 #include <memory>
 
-#if 0
 template<typename T, auto Deleter>
 struct GenericDeleter
 {
   void operator()(T *rc) const
   {
+    LOG << "generic deleter";
     Deleter(rc);
   }
 };
 
 template<typename T, auto Func>
 using legacy_ptr = std::unique_ptr<T, GenericDeleter<T, Func>>;
-#endif
 
 struct Type
 {
-  int integer{0};
-  float floatNum{.0};
+  int integer;
+  float floatNum;
 };
 
+static Type* init()
+{
+  return new Type;
+}
+
+static void free_type(Type *type)
+{
+  LOG << "free_type";
+  delete type;
+}
+
+static void free_type_2(Type *type)
+{
+}
+
+struct FreeTypeFunctor
+{
+  void operator()(Type *type)
+  {
+    LOG << "typed deleter";
+    free_type(type);
+  }
+};
+
+using Type_uptr = std::unique_ptr<Type, decltype(&free_type)>;
+using Type_uptr_2 = std::unique_ptr<Type, FreeTypeFunctor>;
+using Type_uptr_3 = legacy_ptr<Type, free_type>;
+using Type_uptr_4 = legacy_ptr<Type, free_type_2>;
+
+
+static void use_uptr(Type_uptr uptr)
+{
+  LOG << "use_uptr";
+}
+
+static void use_type_by_ref(Type &ref)
+{
+  LOG << "use by ref";
+}
 
 int main()
 {
   Type *ptr {nullptr};
   LOG << "sizeof(ptr): " << sizeof(ptr);
 
-  std::unique_ptr<Type> uptr;
+  Type_uptr uptr(new Type, &free_type);
+
+  Type_uptr_2 uptr_2(new Type);
+  Type_uptr_3 uptr_3(new Type);
+
   LOG << "sizeof(uptr): " << sizeof(uptr);
+  LOG << "sizeof(uptr_2): " << sizeof(uptr_2);
+  LOG << "sizeof(uptr_3): " << sizeof(uptr_3);
+//  use_uptr(std::move(uptr)); 
 
   return 0;
 }
